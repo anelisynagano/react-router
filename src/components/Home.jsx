8e78498b8e133dc8069008b16717b4a5672b2a88
Anelisy Nagano <anelisynagano@gmail.com>
2019-04-29 11:30:51 +0100

    start repo

---
+++ src/components/Home.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+class Home extends Component {
+    render() {
+        return (
+            <div>
+                Welcome to Home
+            </div>
+        );
+    }
+}
+
+export default Home;
